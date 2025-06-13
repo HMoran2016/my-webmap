1686ec9afe4130dd2e740ac7258655e5ae06d5ff
HMoran2016 <111935960+HMoran2016@users.noreply.github.com>
2025-06-13 10:26:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-
+// Initialize the map
 var map = L.map('map').setView([27.9944, -81.7603], 7);
 
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+// Define base layers
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+});
+
+var esriSat = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, Earthstar Geographics',
   maxZoom: 19
-}).addTo(map);
+});
 
+// Add one of them to start
+osm.addTo(map);
+
+// Add basemap toggle control
+var baseMaps = {
+  "OpenStreetMap": osm,
+  "Esri Satellite": esriSat
+};
+L.control.layers(baseMaps).addTo(map);
+
+// Add GeoJSON sample data
 var geojsonFeature = {
   "type": "Feature",
   "geometry": {
